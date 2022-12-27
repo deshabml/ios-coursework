84b23e9616a1b2c802be8392eec6380ed936f9e1
deshabml <deshab@yandex.ru>
2022-12-27 19:16:05 +0300

    Created Info and Habits ViewController

---
+++ MyHabits/Habits/HabitsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HabitsViewController.swift
+//  MyHabits
+//
+//  Created by Лаборатория on 27.12.2022.
+//
+
+import UIKit
+
+class HabitsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+}
---
+++ MyHabits/Info/InfoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  InfoViewController.swift
+//  MyHabits
+//
+//  Created by Лаборатория on 27.12.2022.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+
+    }
+
+}
