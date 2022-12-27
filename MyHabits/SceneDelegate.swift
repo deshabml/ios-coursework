414f9efa3c7750d0da1c4c5fa081b30a48d84c0d
deshabml <deshab@yandex.ru>
2022-12-27 19:40:07 +0300

    Created icons

--- MyHabits/SceneDelegate.swift
+++ MyHabits/SceneDelegate.swift
@@ -11,11 +11,34 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
+    func createFirstController() -> UINavigationController {
+        let fvc = UINavigationController(rootViewController: HabitsViewController())
+        fvc.tabBarItem = UITabBarItem(title: "Привычки", image: UIImage(systemName: "rectangle.grid.1x2.fill"), tag: 0)
+        return fvc
+    }
+
+    func createSecondController() -> UINavigationController {
+        let lvc = UINavigationController(rootViewController: InfoViewController())
+        lvc.tabBarItem = UITabBarItem(title: "Информация", image: UIImage(systemName: "info.circle.fill"), tag: 1)
+        return lvc
+    }
+
+    func creatTabBar() -> UITabBarController {
+        let tabBar = UITabBarController()
+        tabBar.viewControllers = [createFirstController(), createSecondController()]
+        tabBar.tabBar.backgroundColor = UIColor(
+            red: 247/255,
+            green: 247/255,
+            blue: 247/255,
+            alpha: 1.0)
+        tabBar.tabBar.tintColor = UIColor(named: "ColorSet")
+        return tabBar
+    }
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let winScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: winScene)
-        window.rootViewController = ViewController()
+        window.rootViewController = creatTabBar()
         window.makeKeyAndVisible()
 
         self.window = window
