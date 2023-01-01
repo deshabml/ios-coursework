afdd0cfa03803c05aec984c8e1bb2b379370b056
deshabml <deshab@yandex.ru>
2023-01-01 15:35:02 +0300

    Created labelName

--- MyHabits/Habits/HabitViewController.swift
+++ MyHabits/Habits/HabitViewController.swift
@@ -9,17 +9,45 @@ import UIKit
 
 class HabitViewController: UIViewController {
 
+    private lazy var labelName: UILabel = {
+        let labelName = UILabel()
+        labelName.text = "НАЗВАНИЕ"
+        labelName.font = UIFont.systemFont(ofSize: 13, weight: .semibold)
+        labelName.tintColor = .black
+        labelName.translatesAutoresizingMaskIntoConstraints = false
+        return labelName
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        view.addSubviews([
+            labelName
+        ])
+        installingСonstraints()
     }
 
     override func viewWillAppear(_ animated: Bool) {
         navigationBarSetting()
     }
 
+}
+
+extension HabitViewController {
+
+    private func installingСonstraints() {
+        NSLayoutConstraint.activate([
+            labelName.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 21),
+            labelName.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 21)
+        ])
+    }
+
     @objc func buttonAction() {
         print("ap op")
+    }
+
+    @objc func buttenBackAction() {
+        dismiss(animated: true)
     }
 
     private func navigationBarSetting() {
@@ -27,15 +55,25 @@ class HabitViewController: UIViewController {
         let novigationBarColor = UINavigationBarAppearance()
         novigationBarColor.titleTextAttributes = [.foregroundColor: UIColor.black]
         novigationBarColor.backgroundColor = UIColor(
-            red: 247/255,
-            green: 247/255,
-            blue: 247/255,
-            alpha: 1.0)
+            red: 249/255,
+            green: 249/255,
+            blue: 249/255,
+            alpha: 0.94)
         navigationItem.scrollEdgeAppearance = novigationBarColor
         let buttenSave = UIBarButtonItem(title: "Cохранить", style: .done, target: self, action: #selector(buttonAction))
-        let buttenBack = UIBarButtonItem(title: "Отменить", style: .done, target: self, action: #selector(buttonAction))
+        let buttenBack = UIBarButtonItem(title: "Отменить", style: .done, target: self, action: #selector(buttenBackAction))
         navigationItem.rightBarButtonItem = buttenSave
         navigationItem.leftBarButtonItem = buttenBack
+        navigationItem.rightBarButtonItem?.tintColor = UIColor(
+            red: 161/255,
+            green: 22/255,
+            blue: 204/255,
+            alpha: 1.0)
+        navigationItem.leftBarButtonItem?.tintColor = UIColor(
+            red: 161/255,
+            green: 22/255,
+            blue: 204/255,
+            alpha: 1.0)
     }
 
 }
